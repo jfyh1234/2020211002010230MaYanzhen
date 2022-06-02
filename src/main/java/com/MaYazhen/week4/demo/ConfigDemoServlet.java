9ec6f34c6821c40dcb29e4c2da157a7ad5c08f9d
jfyh1234 <2744260460@qq.com>
2022-06-02 11:19:41 +0800

    week14

---
+++ src/main/java/com/MaYazhen/week4/demo/ConfigDemoServlet.java
@@ -0,0 +1,28 @@
+package com.MaYazhen.week4.demo;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="ConfigDemoServlet",urlPatterns="/jdbc",initParams={
+        @WebInitParam(name="name",value="MaYazhen"),
+        @WebInitParam(name="student-id",value="2019211001001201")
+})
+
+public class ConfigDemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        doPost(request,response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer=response.getWriter();
+        writer.println("name:"+getServletConfig().getInitParameter("name"));
+        writer.println("student-id:"+getServletConfig().getInitParameter("student-id"));
+    }
+}
